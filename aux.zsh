7e6468c6db6568485c46fdd60c69171f0eac54bf
Mikko Aaltonen <lede@krypta.fi>
2026-01-22 13:33:23 +0200

    pyenv path

--- aux.zsh
+++ aux.zsh
@@ -20,6 +20,7 @@ fi
 ## PYENV
 if command -v pyenv &>/dev/null; then
   export PYENV_ROOT=$HOME/.local/share/pyenv
+  export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init - zsh)"
   eval "$(pyenv virtualenv-init -)"
 fi
